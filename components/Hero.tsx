300ad27c1a8734eaebbc89766263230151b34cdd
Ardit Nimanaj <ardit.nimanaj@pabau.com>
2025-03-16 15:26:10 +0100

    updated the logo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,17 +4,17 @@ export default function Hero() {
     <div className="relative w-full h-[500px] flex items-center justify-center overflow-hidden">
       {/* Background Video */}
       <video
-        className="absolute inset-0 w-full h-full object-cover opacity-50"
+        className="absolute inset-0 w-full h-full object-cover opacity-70"
         autoPlay
         loop
         muted
         playsInline
       >
-        <source src="../videos/banner1.mp4" type="video/mp4" />
+        <source src="../videos/banner.mp4" type="video/mp4" />
       </video>
 
       {/* Black Overlay with Text */}
-      <div className="absolute bg-black bg-opacity-70 text-white px-8 py-4 rounded-lg">
+      <div className="absolute bg-black bg-opacity-70 text-white px-8 py-4">
         <h1 className="text-4xl font-bold text-center">Shala Body Shop</h1>
         <p className="text-lg text-center mt-2">Quality repairs, exceptional service.</p>
       </div>
